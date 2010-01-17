cab85425a2967439badb3c411383700dfbf5abb8
Ivan Voras <ivoras@gmail.com>
2010-01-17 01:59:22 +0100

    Encrypted notepad development

---
+++ src/enotes/Main.java
@@ -0,0 +1,29 @@
+/*
+ * (c) 2009. Ivan Voras <ivoras@fer.hr>
+ */
+
+package enotes;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author ivoras
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {}
+
+        MainForm mf = new MainForm();
+        mf.setLocationRelativeTo(null);
+        mf.setVisible(true);
+
+    }
+
+}
---
+++ src/enotes/SaveMetadata.java
@@ -0,0 +1,16 @@
+/*
+ * (c) 2009. Ivan Voras <ivoras@fer.hr>
+ */
+
+package enotes;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ivoras
+ */
+public class SaveMetadata implements Serializable {
+    public long     timestamp;
+    public String   username;
+}
