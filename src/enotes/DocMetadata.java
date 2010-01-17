8e9544c9a99360abdeb8349e0a18225fe325b9da
ivoras <ivoras@betelgeuse.cosmos>
2010-01-17 20:52:25 +0100

    Implemented file loading

--- src/enotes/DocMetadata.java
+++ src/enotes/DocMetadata.java
@@ -4,6 +4,9 @@
 
 package enotes;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 /**
@@ -15,15 +18,30 @@ public class DocMetadata {
     static final byte[] SIGNATURE = { 0x00, (byte)0xff, (byte)0xed, (byte)0xed };
     static final byte VERSION_FORMAT = 1;
     static final byte VERSION_MINOR = 0;
-    static final byte[] DEFAULT_IV = new byte[] {
-        (byte)0xfE, 0x14, 0x39, (byte)0xaC, 0x0e, 0x72, (byte)0x9F, 0x5b,
-        (byte)0xef, 0x01, 0x42, (byte)0xd1, 0x40, 0x12, (byte)0x8F, 0x17
-    };
 
     public ArrayList<SaveMetadata> saveHistory = new ArrayList<SaveMetadata>();
     public boolean modified = false;
     public String filename;
     public int caretPosition;
     public byte[] key;
-    
+
+    void saveMetadata(ObjectOutputStream oout) throws IOException {
+        oout.writeInt(caretPosition);
+        oout.writeUTF(filename);
+        oout.writeInt(saveHistory.size());
+        for (SaveMetadata sm: saveHistory) {
+            oout.writeLong(sm.timestamp);
+            oout.writeUTF(sm.username);
+        }
+    }
+
+    void loadMetadata(ObjectInputStream ois) throws IOException {
+        caretPosition = ois.readInt();
+        filename = ois.readUTF();
+        int nSave = ois.readInt();
+        saveHistory = new ArrayList<SaveMetadata>();
+        for (int i = 0; i < nSave; i++)
+            saveHistory.add(new SaveMetadata(ois.readLong(), ois.readUTF()));
+    }
+
 }
