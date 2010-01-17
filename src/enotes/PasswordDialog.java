61da491933dea11e3af5a3254924bb11ad35dcd1
Ivan Voras <ivoras@gmail.com>
2010-01-17 21:26:39 +0100

    Add search highlighter

--- src/enotes/PasswordDialog.java
+++ src/enotes/PasswordDialog.java
@@ -1,7 +1,8 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * (c) 2009. Ivan Voras <ivoras@fer.hr>
+ * Released under the 2-clause BSDL.
  */
+
 
 /*
  * PasswordDialog.java
