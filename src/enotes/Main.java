5c5fde31f0a17720376488505ec451283c6c4c85
Ivan Voras <ivoras@gmail.com>
2010-01-28 12:17:07 +0100

    Fix folder filter

--- src/enotes/Main.java
+++ src/enotes/Main.java
@@ -14,7 +14,7 @@ import javax.swing.UIManager;
  */
 public class Main {
 
-    static final String VERSION = "1.0beta3";
+    static final String VERSION = "1.0beta4";
 
     /**
      * @param args the command line arguments
