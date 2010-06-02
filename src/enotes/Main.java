4f41fed0ec62beff9419be39d8b37b2e7476f0db
Ivan Voras <ivoras@gmail.com>
2010-06-03 01:48:47 +0200

    Advance version to beta6

--- src/enotes/Main.java
+++ src/enotes/Main.java
@@ -14,7 +14,7 @@ import javax.swing.UIManager;
  */
 public class Main {
 
-    static final String VERSION = "1.0beta5";
+    static final String VERSION = "1.0beta6";
 
     /**
      * @param args the command line arguments
